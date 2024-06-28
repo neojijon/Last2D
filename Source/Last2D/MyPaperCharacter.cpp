506338c8b0da03732bc31aa7d578185cdd962f30
neojijon <neojijon@gmail.com>
2024-06-28 13:52:40 +0900

    수정

--- Source/Last2D/MyPaperCharacter.cpp
+++ Source/Last2D/MyPaperCharacter.cpp
@@ -1,4 +1,109 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+#include "MyPaperCharacter.h"
+#include "Engine/LocalPlayer.h"
 
-#include "MyPaperCharacter.h
+//컴퍼넌트
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "PaperFlipbookComponent.h"
+
+//Input
+#include "GameFramework/Controller.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "InputActionValue.h"
+
+
+AMyPaperCharacter::AMyPaperCharacter()
+{
+    //카메라, 스프링암 컴퍼넌트를 값 세팅
+    // Spring Arm Component
+    SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+    SpringArm->SetupAttachment(RootComponent);
+    SpringArm->TargetArmLength = 500.0f;
+    SpringArm->SetRelativeRotation(FRotator(-10.0f, -90.0f, 0.0f)); //횡스크롤 화면 
+    //SpringArm->SetWorldRotation(FRotator(0.0f, 0.0f, -90.0f));
+    SpringArm->bDoCollisionTest = false;
+    SpringArm->bInheritPitch = false;
+    SpringArm->bInheritYaw = false;
+    SpringArm->bInheritRoll = false;
+
+    // Camera Component
+    Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("SideViewCamera"));
+    Camera->SetupAttachment(SpringArm, USpringArmComponent::SocketName);
+    Camera->bUsePawnControlRotation = false; // Camera does not rotate relative to arm
+
+    PrimaryActorTick.bCanEverTick = true;
+    bIsAttacking = false;
+
+}
+
+void AMyPaperCharacter::BeginPlay()
+{
+    Super::BeginPlay();
+
+    bIsAttacking = false;
+    GetSprite()->SetFlipbook(FB_Char_Idle);
+
+    //GetSprite 이벤트 함수 등록
+    GetSprite()->OnFinishedPlaying.AddDynamic(this, &AMyPaperCharacter::OnAttackFinished);
+}
+
+
+void AMyPaperCharacter::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+
+    //추가 구현하면 됨.
+    //UpdateAnimation();
+
+}
+
+
+void AMyPaperCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+    Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+    if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+    {
+        EnhancedInputComponent->BindAction(IA_Move.Get(), ETriggerEvent::Triggered, this, &AMyPaperCharacter::Move);
+        EnhancedInputComponent->BindAction(IA_Attack.Get(), ETriggerEvent::Triggered, this, &AMyPaperCharacter::Attack);
+    }
+
+}
+
+
+void AMyPaperCharacter::Move(const FInputActionValue& Value)
+{
+    FVector2D MovementVector = Value.Get<FVector2D>();
+    AddMovementInput(FVector(MovementVector.X, 0.0f, 0.0f));
+
+    UE_LOG(LogTemp, Warning, TEXT("EnhancedInputComponent"));
+}
+
+void AMyPaperCharacter::Attack(const FInputActionValue& Value)
+{
+    // 공격 로직을 여기에 구현
+    if (!bIsAttacking)
+    {
+        bIsAttacking = true;
+        GetSprite()->SetFlipbook(FB_Char_Attack01);
+        GetSprite()->SetLooping(false);
+        GetSprite()->PlayFromStart();
+
+        UE_LOG(LogTemp, Warning, TEXT("Attack!"));
+    }
+}
+
+
+void OnAttackFinished()
+{
+
+}
+
+
+
+
--- Source/Last2D/MyPaperCharacter.h
+++ Source/Last2D/MyPaperCharacter.h
@@ -2,9 +2,16 @@
 
 #pragma once
 
-#include "CoreMinimal.h"
+//#include "CoreMinimal.h"
 #include "PaperCharacter.h"
 #include "MyPaperCharacter.generated.h"
+
+
+class USpringArmComponent;
+class UCameraComponent;
+class UInputMappingContext;
+class UInputAction;
+struct FInputActionValue;
 
 /**
  * 
@@ -13,5 +20,72 @@ UCLASS()
 class LAST2D_API AMyPaperCharacter : public APaperCharacter
 {
 	GENERATED_BODY()
-	
+
+
+	/** Camera boom positioning the camera behind the character */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	USpringArmComponent* SpringArm;
+
+	/** Follow camera */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	UCameraComponent* Camera;
+
+	/** MappingContext */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputMappingContext* DefaultMappingContext;
+
+	/** IA_Attack; Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<class UInputAction> IA_Attack;;
+
+	/** Move Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<class UInputAction> IA_Move;
+
+	/** Look Input Action */
+	//UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	//UInputAction* LookAction;
+
+		// Flipbook Animations
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Animations", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<class UPaperFlipbook> FB_Char_Idle;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Animations", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<class UPaperFlipbook> FB_Char_Run;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Animations", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<class UPaperFlipbook> FB_Char_Attack01;
+
+public:
+	AMyPaperCharacter();
+
+protected:
+	// APawn interface
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	// To add mapping context
+	virtual void BeginPlay();
+	virtual void Tick(float DeltaTime) override;
+
+
+public:
+	//이동 Input 이벤트
+	void Move(const FInputActionValue& Value);
+	//공격 Input 이벤트
+	UFUNCTION()	void Attack(const FInputActionValue& Value);
+
+	UFUNCTION()	void OnAttackFinished();	
+	//void UpdateAnimation();
+
+
+	// Movement state
+	bool bIsAttacking;
+	FVector2D MovementInput;
+
+public:
+	/** Returns CameraBoom subobject **/
+	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return SpringArm; }
+	/** Returns FollowCamera subobject **/
+	FORCEINLINE class UCameraComponent* GetFollowCamera() const { return Camera; }
+
 };
