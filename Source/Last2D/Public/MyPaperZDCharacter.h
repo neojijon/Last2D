996fdd53d16669710ef97508f2a1aca9fdf1cfa7
neojijon <neojijon@gmail.com>
2024-06-30 00:36:29 +0900

    PaperZD 사용하기

---
+++ Source/Last2D/Public/MyPaperZDCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PaperZDCharacter.h"
+#include "MyPaperZDCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LAST2D_API AMyPaperZDCharacter : public APaperZDCharacter
+{
+	GENERATED_BODY()
+	
+};
